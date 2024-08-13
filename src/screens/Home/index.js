41aa56756b34ef957382b0c0d5306c95d292a811
edu2107 <750spedu@gmail.com>
2024-08-13 14:36:56 -0300

    filmes att

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -3,9 +3,36 @@ import BannerMovies from '../../components/bannerFilmes';
 import CardMovies from '../../components/cardMovies';
 import Header from '../../components/header';
 import SearchBar from '../../components/searchbar';
-import Filmes from '../../data/movies'
+import Filmes from '../../data/movies';
+import { useState , useEffect } from 'react';
 
 export default function HomeIndex() {
+    const [movies,setFilmes] = useState();
+
+    useEffect(() => {
+
+      async function Filmes() {
+        try{
+        const response = await fetch('https://api.themoviedb.org/3/movie/now_playing?api_key=af4667b75e17eeffcfbd6d1995bb5e77&language=pt-br-BR&page=1')
+        const data = await response.json();
+        console.log(data.results)
+
+        setFilmes(data.results)
+        }
+        catch(error){
+          console.log('erro ao buscar filmes',error)
+        }
+        }
+
+        Filmes();
+    } , [])
+
+
+
+
+
+
+
   return (
     <View style={styles.container}>
      <Header></Header>
@@ -19,14 +46,14 @@ export default function HomeIndex() {
           <FlatList
             horizontal={true}
             showsHorizontalScrollIndicator={false}
-            data={Filmes}
+            data={movies}
             keyExtractor={(item) => item.id}
             renderItem={({ item }) => (
               
               <CardMovies
-                titulo={item.nome}
-                imagem={item.imagem}
-                nota={item.nota}
+                titulo={item.title}
+                imagem={item.poster_path}
+                nota={item.vote_average}
               />
             )}
           />
